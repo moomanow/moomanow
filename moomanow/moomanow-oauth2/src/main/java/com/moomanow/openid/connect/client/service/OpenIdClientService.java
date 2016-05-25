12202d3a6b7a948f1054081d60e7eb9ab2825fb8
kwan <jarupong.w@morethanmatcher.com>
2016-05-25 15:32:09 +0700

    fix pom

---
+++ moomanow/moomanow-oauth2/src/main/java/com/moomanow/openid/connect/client/service/OpenIdClientService.java
@@ -0,0 +1,16 @@
+package com.moomanow.openid.connect.client.service;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+public interface OpenIdClientService {
+
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)throws AuthenticationException, IOException, ServletException;
+
+}
