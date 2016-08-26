4ddd9686f08c4965bb606e8b6f5389cbff56c034
kwan <jarupong.w@morethanmatcher.com>
2016-08-26 18:16:09 +0700

    Add MDC

--- moomanow/moomanow-logger/src/main/java/com/moomanow/logger/MDC.java
+++ moomanow/moomanow-logger/src/main/java/com/moomanow/logger/MDC.java
@@ -1,26 +1,76 @@
 
 package com.moomanow.logger;
 
+import java.io.Serializable;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
-public final class MDC {
+public final class MDC implements Serializable{
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3908803341329013252L;
 
-    private MDC() {
-    }
+	private static Method put = getMethod("put", String.class, Object.class);
+	private static Method get = getMethod("get", String.class);
+	private static Method remove = getMethod("remove", String.class);
+	private static Method getMap = getMethod("getMap");
 
-    public static Object put(String key, Object val) {
-        return LoggerProviderFactory.PROVIDER.putMdc(key, val);
-    }
 
-    public static Object get(String key) {
-        return LoggerProviderFactory.PROVIDER.getMdc(key);
-    }
+	private static Method getMethod(String name, Class<?>... parameterTypes) {
+		List<String> lList = Arrays.asList(new String[] { "org.jboss.logging.MDC", "org.apache.log4j.MDC" });
+		Class<?> clazz = null;
+		try {
+			for (String class1 : lList) {
+				try{
+					clazz = Class.forName(class1);
+					break;
+				}catch (Exception e) {
+				}
+				
+			}
+			if(clazz == null) return null;
+			return clazz.getMethod(name, parameterTypes);
+		} catch (NoSuchMethodException | SecurityException e) {
+//			e.printStackTrace();
+		}
+		return null;
 
-    public static void remove(String key) {
-    	LoggerProviderFactory.PROVIDER.removeMdc(key);
-    }
+	}
 
-    public static Map<String, Object> getMap() {
-        return LoggerProviderFactory.PROVIDER.getMdcMap();
-    }
+	public static Object put(String key, Object val) {
+		try {
+			return put.invoke(null, key, val);
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+		}
+		return null;
+	}
+
+	public static Object get(String key) {
+		try {
+			return get.invoke(null, key);
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+		}
+		return null;
+	}
+
+	public static void remove(String key) {
+		try {
+			remove.invoke(null, key);
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+		}
+	}
+
+	public static Map<String, Object> getMap() {
+		try {
+			return (Map<String, Object>) getMap.invoke(null);
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+		}
+		return null;
+	}
 }
---
+++ moomanow/moomanow-logger/src/test/java/com/moomanow/logger/TestMDC.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package com.moomanow.logger;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author jm
+ *
+ */
+public class TestMDC {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		
+		MDC.put("sss","wwwww");
+//		fail("Not yet implemented");
+	}
+
+}
