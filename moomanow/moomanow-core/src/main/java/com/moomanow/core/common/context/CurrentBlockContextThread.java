dedf112040feecb0618cef4dc74287526f93945c
kwan <kwan@localhost>
2017-05-02 12:47:07 +0700

    change version 0.1.0

--- moomanow/moomanow-core/src/main/java/com/moomanow/core/common/context/CurrentBlockContextThread.java
+++ moomanow/moomanow-core/src/main/java/com/moomanow/core/common/context/CurrentBlockContextThread.java
@@ -4,6 +4,7 @@
 package com.moomanow.core.common.context;
 
 import com.moomanow.core.common.processhandler.BlockContext;
+import com.moomanow.core.common.processhandler.Context;
 
 /**
  * @author Jaurpong.w(Kwan)
@@ -18,8 +19,22 @@ public class CurrentBlockContextThread {
 		return blockContextThreadLocal.get();
 	}
 	
-	public static <T> T getContext(Class<T> class1){
-		return blockContextThreadLocal.get().getContext(class1);
+	public static <T extends Context> T getContext(Class<T> class1){
+		BlockContext blockContext = getBlockContext();
+		if(blockContext==null){
+			blockContext = new BlockContext();
+			blockContextThreadLocal.set(blockContext);
+		}
+		T t = blockContext.getContext(class1);
+		if(t==null){
+			try {
+				t = class1.newInstance();
+				blockContext.setContext(t);
+			} catch (InstantiationException | IllegalAccessException e) {
+				e.printStackTrace();
+			}
+		}
+		return blockContext.getContext(class1);
 	}
 	
 	public static void setBlockContext(BlockContext blockContext){
